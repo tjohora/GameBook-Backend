844bc2544c0d6cec59b4c8e1f9d0989a02b520d4
TJ <TJ@LAPTOP-9BNN91K9.Home>
2020-03-11 21:47:04 +0000

    Renamed stuff

--- test/UnitTests/PostDAOTest.java
+++ test/UnitTests/PostDAOTest.java
@@ -3,8 +3,9 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package DAOs;
+package UnitTests;
 
+import DAOs.PostDAO;
 import DTOs.Post;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
